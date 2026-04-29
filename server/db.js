8ef94521da24fe924078614f8cf062ad32a3224f
Jai Coxon <jcoxon@pulsemining.com.au>
2026-04-29 12:20:04 +1000

    update 2.0

--- server/db.js
+++ server/db.js
@@ -1,11 +1,19 @@
 const { Pool } = require('pg');
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL environment variable is not set');
+}
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
   max: 1,
   idleTimeoutMillis: 10000,
   connectionTimeoutMillis: 10000,
+});
+
+pool.on('error', (err) => {
+  console.error('pg pool error:', err.message);
 });
 
 module.exports = pool;
